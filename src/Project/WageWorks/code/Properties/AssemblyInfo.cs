7d66e89f566e9900a09e05fe22bc99a5d335f6d2
Vidhyabalu <vidhya.balasubramanian@xcentium.com>
2018-12-05 12:13:29 -0500

    changes to references

--- src/Project/WageWorks/code/Properties/AssemblyInfo.cs
+++ src/Project/WageWorks/code/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Vista.Project.SpeerAmmo")]
+[assembly: AssemblyTitle("Wageworks.Project.Wageworks")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Vista.Project.SpeerAmmo")]
+[assembly: AssemblyProduct("Wageworks.Project.Wageworks")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
